a1fe74818300017a46af78a5e184d8e35b6cc954
Evgenii Vlasov <senzik@gmail.com>
2023-06-10 13:44:44 +0300

    first Try

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,11 @@
+package org.example;
+import lombok.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Human {
+    String firstName;
+    String lastName;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Human bob = new Human("Bob","Ferguson");
+        System.out.println(bob.toString());
+    }
+}
